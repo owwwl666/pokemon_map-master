ef1dc58da9623b1c98ff93b17ba6ffdffc392713
owwwl666 <zaharsalam228@gmail.com>
2023-06-24 02:09:39 +1000

    register Pokemon model on admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models  # noqa F401
 
+
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200,blank=True)
+    title = models.CharField(max_length=200, blank=True)
