90718858f69d0e3ac7c938a92bbd1997f177092a
owwwl666 <zaharsalam228@gmail.com>
2023-06-24 06:40:19 +1000

    show pokemons on mainpage.
    show pokemons on mainpage.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-
+from pokemon_entities.models import Pokemon
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -30,6 +30,8 @@ def show_all_pokemons(request):
     with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
         pokemons = json.load(database)['pokemons']
 
+    admin_from_pokemons = Pokemon.objects.all()
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
         for pokemon_entity in pokemon['entities']:
@@ -40,11 +42,11 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in admin_from_pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else None,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
@@ -56,7 +58,6 @@ def show_all_pokemons(request):
 def show_pokemon(request, pokemon_id):
     with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
         pokemons = json.load(database)['pokemons']
-
     for pokemon in pokemons:
         if pokemon['pokemon_id'] == int(pokemon_id):
             requested_pokemon = pokemon
