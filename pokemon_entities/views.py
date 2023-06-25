fa7b59f1295b796f9b00ea3bfc331b13839ef0ff
owwwl666 <zaharsalam228@gmail.com>
2023-06-26 06:42:01 +1000

    Update views.py
    added pokemon names in other languages

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -65,6 +65,8 @@ def show_pokemon(request, pokemon_id):
         pokemon = {
             "pokemon_id": pokemon_id,
             "title_ru": requested_pokemon.title,
+            "title_en": requested_pokemon.title_en,
+            "title_jp": requested_pokemon.title_jp,
             "description": requested_pokemon.description,
             "img_url": request.build_absolute_uri(requested_pokemon.image.url)
             if requested_pokemon.image else None
