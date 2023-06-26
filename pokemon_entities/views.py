8bbed55a4d1297e03ec001f0a538067d28b4a9d4
owwwl666 <zaharsalam228@gmail.com>
2023-06-26 22:28:52 +1000

    Update views.py
    dried code

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,5 @@
 import folium
-import json
 import datetime
-
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from pokemon_entities.models import Pokemon, PokemonEntity
 from django.utils import timezone
@@ -11,27 +8,21 @@ timezone.localtime(timezone.now())
 now = datetime.datetime.now()
 
 MOSCOW_CENTER = [55.751244, 37.618423]
-DEFAULT_IMAGE_URL = (
-    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
-    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
-    '&fill=transparent'
-)
 
 
-def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+def add_pokemon(folium_map, lat, lon, image_url):
     icon = folium.features.CustomIcon(
         image_url,
         icon_size=(50, 50),
     )
     folium.Marker(
         [lat, lon],
-        # Warning! `tooltip` attribute is disabled intentionally
-        # to fix strange folium cyrillic encoding bug
         icon=icon,
     ).add_to(folium_map)
 
 
 def show_all_pokemons(request):
+    """Отображает всех покемонов на главной странице."""
     pokemons = Pokemon.objects.all()
     pokemons_entities = PokemonEntity.objects.filter(appeared_at__lte=now, disappeared_at__gte=now)
 
@@ -41,14 +32,13 @@ def show_all_pokemons(request):
             folium_map, pokemon_entity.lat,
             pokemon_entity.lon,
             request.build_absolute_uri(pokemon_entity.pokemon.image.url)
-            if pokemon_entity.pokemon.image else None
         )
 
     pokemons_on_page = []
     for pokemon in pokemons:
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
-            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else None,
+            'img_url': request.build_absolute_uri(pokemon.image.url),
             'title_ru': pokemon.title,
         })
 
@@ -59,39 +49,35 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
+    """На отдельной странице выводит информацию о каждом покемоне."""
     requested_pokemon = Pokemon.objects.get(id=pokemon_id)
-    if requested_pokemon:
-        pokemon = {
-            "pokemon_id": pokemon_id,
-            "title_ru": requested_pokemon.title,
-            "title_en": requested_pokemon.title_en,
-            "title_jp": requested_pokemon.title_jp,
-            "description": requested_pokemon.description,
-            "img_url": request.build_absolute_uri(requested_pokemon.image.url) \
-                if requested_pokemon.image else None,
-            "previous_evolution": {"pokemon_id": requested_pokemon.previous_evolution_id,
-                                   "title_ru": requested_pokemon.previous_evolution.title,
-                                   "img_url": request.build_absolute_uri(requested_pokemon.previous_evolution.image.url)
-                                   } if requested_pokemon.previous_evolution else None,
+    pokemon = {
+        "pokemon_id": pokemon_id,
+        "title_ru": requested_pokemon.title,
+        "title_en": requested_pokemon.title_en,
+        "title_jp": requested_pokemon.title_jp,
+        "description": requested_pokemon.description,
+        "img_url": request.build_absolute_uri(requested_pokemon.image.url),
+        "previous_evolution": {"pokemon_id": requested_pokemon.previous_evolution_id,
+                               "title_ru": requested_pokemon.previous_evolution.title,
+                               "img_url": request.build_absolute_uri(requested_pokemon.previous_evolution.image.url)
+                               }
 
-        }
+    }
 
-        try:
-            next_evolution = requested_pokemon.pokemon_set.all()[0]
-        except IndexError:
-            pass
-        else:
-            pokemon.update({
-                "next_evolution": {"pokemon_id": next_evolution.id,
-                                   "title_ru": next_evolution.title,
-                                   "img_url": request.build_absolute_uri(
-                                       next_evolution.image.url)
-
-                                   }
-            }
-            )
+    try:
+        next_evolution = requested_pokemon.pokemon_set.all()[0]
+    except IndexError:
+        pass
     else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+        pokemon.update({
+            "next_evolution": {"pokemon_id": next_evolution.id,
+                               "title_ru": next_evolution.title,
+                               "img_url": request.build_absolute_uri(
+                                   next_evolution.image.url)
+                               }
+        }
+        )
 
     pokemon_entities = PokemonEntity.objects.filter(pokemon__title=requested_pokemon.title, appeared_at__lte=now,
                                                     disappeared_at__gte=now)
@@ -102,7 +88,6 @@ def show_pokemon(request, pokemon_id):
             folium_map, pokemon_entity.lat,
             pokemon_entity.lon,
             request.build_absolute_uri(pokemon_entity.pokemon.image.url)
-            if pokemon_entity.pokemon.image else None
         )
 
     return render(request, 'pokemon.html', context={
