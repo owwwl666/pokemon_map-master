ff0c0b007998d381656017874b14885b384c9c72
owwwl666 <zaharsalam228@gmail.com>
2023-06-26 10:36:50 +1000

    Update models.py
    added pokemon evolution field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,9 +1,13 @@
-from django.db import models  # noqa F401
+from django.db import models
 
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images', null=True)
+    previous_evolution = models.ForeignKey("Pokemon", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
