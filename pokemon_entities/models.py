34af9db5ab3452ea4965e9ff5d43aef308215cdc
owwwl666 <zaharsalam228@gmail.com>
2023-06-26 21:09:40 +1000

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,7 +7,7 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images', null=True)
-    previous_evolution = models.ForeignKey("Pokemon", on_delete=models.CASCADE, blank=True, null=True)
+    previous_evolution = models.ForeignKey("Pokemon", on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
