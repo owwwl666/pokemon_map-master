c9e88e3b88c41895df1383835b9377cb27225b7e
owwwl666 <zaharsalam228@gmail.com>
2023-06-28 06:41:44 +1000

    Update models.py
    change related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,7 +8,7 @@ class Pokemon(models.Model):
     description = models.TextField(blank=True, verbose_name="Описание")
     image = models.ImageField(upload_to='images', verbose_name="Картинка")
     previous_evolution = models.ForeignKey("Pokemon", on_delete=models.SET_NULL, blank=True, null=True,
-                                           verbose_name="Предок", related_name='next_evolution')
+                                           verbose_name="Из кого эволюционировал", related_name='next_evolutions')
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон",
-                                related_name='catching_pokemon')
+                                related_name='map_marked_pokemons')
     lat = models.FloatField(verbose_name="Местоположение (Широта)")
     lon = models.FloatField(verbose_name="Местоположение (Долгота)")
     appeared_at = models.DateTimeField(verbose_name="Время появления на карте")
