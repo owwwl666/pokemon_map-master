cdb33bfbab870f85f9ebaceb7d6034ff9d384604
owwwl666 <zaharsalam228@gmail.com>
2023-06-30 20:14:12 +1000

    Update models.py
    change related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -16,7 +16,7 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон",
-                                related_name='pokemon_entities')
+                                related_name='entities')
     lat = models.FloatField(verbose_name="Местоположение (Широта)")
     lon = models.FloatField(verbose_name="Местоположение (Долгота)")
     appeared_at = models.DateTimeField(verbose_name="Время появления на карте")
